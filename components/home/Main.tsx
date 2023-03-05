e8b85e9e8d61da31c7f2ae9239efba73a40d5ef0
Xiao215 <xiaozhang20030215@gmail.com>
2023-03-04 22:15:18 -0500

    added company

--- components/home/Main.tsx
+++ components/home/Main.tsx
@@ -6,6 +6,9 @@ import Slime from "../decoration/Slime";
 import board from "../../public/assets/decoration/lines.png";
 import ProfilePic from "./ProfilePic";
 import LineChart from "./LineChart";
+import UofT from "../../public/assets/logo/uoft.svg";
+import RBC from "../../public/assets/logo/rbc.svg";
+import Google from "../../public/assets/logo/google.svg";
 const TerminalView = dynamic(() => import("../textfield/TerminalView"), {
   ssr: false,
 });
@@ -40,10 +43,13 @@ const Main: NextPage = () => {
             <Slime width={250} height={150} />
           </div> */}
           <div className="lg:col-span-3 md:col-span-4 text-lg text-atomgray">
-            Previously, I have been doing a lot of Full Stack Development, both
-            during my RBC internship and at hackathons, with
+            In the past, I have worked on many full stack projects based using
             <span className="text-atomred"> TypeScript Nextjs </span>
-            Frontend and Python backend. I have also written many very cool
+            Frontend and <span className="text-atomred">Flask Python </span>
+            backend. My physics reports (maybe I will upload them one day) also
+            utilized <span className="text-atomred">Python</span> to process
+            experiment data. I have also tried building Transformer Model from
+            scratch with it. In high school, I have also written many very cool
             <span className="text-atomred"> Java </span>
             Simulations with OOP. Additionally,
             <span className="text-atomred"> C++ </span> has become my favourious
@@ -64,35 +70,87 @@ const Main: NextPage = () => {
           <span className="absolute left-1/2 -bottom-2 h-[3px] w-1/2 origin-center transform bg-atomred transition-all duration-300"></span>
         </span>
         <div
+          className="md:px-20 py-10 text-lg text-atomgray "
+          data-aos="fade-in"
+        >
+          <Image
+            src={UofT}
+            alt="UofT"
+            height={120}
+            className="md:float-left float-none"
+          />
+          <div>
+            I am a <span className="text-atomcyan">second-year</span> student
+            studying <span className="text-atompurple">Engieering Science</span>{" "}
+            at the
+            <span className="text-atompurple"> University of Toronto</span>,
+            with a major in{" "}
+            <span className="text-atompurple">Machine Intelligence</span>.
+            Through this program, I have been exposed to a wide range of core
+            engineering concepts, including quantum mechanics, molecular
+            biology, and fluid dynamics, as well as developing an engineering
+            mindset and problem-solving skills. <br></br>In addition to my
+            schoolwork, I have been actively engaged in extracurricular
+            activities that have allowed me to develop my skills and contribute
+            to the larger student community. I am a Webmaster for the
+            <span className="text-atomyellow">
+              {" "}
+              University of Toronto Engineering Student Consulting Association{" "}
+            </span>
+            and am involved with the
+            <span className="text-atomyellow"> aUToronto </span> student team,
+            working on autonomous vehicle perception. I have also been actively
+            engaged with the
+            <span className="text-atomyellow">
+              {" "}
+              Google Developer Student Club{" "}
+            </span>
+            , supporting and empowering student developers.
+          </div>
+        </div>
+      </div>
+      <div className="px-20 pt-20">
+        <span className="font-mono text-emerald-300 text-3xl relative">
+          Xiao.Work();
+          <span className="absolute left-0 -bottom-2 h-[3px] w-1/2 origin-center transform bg-atompurple transition-all duration-300"></span>
+          <span className="absolute left-1/2 -bottom-2 h-[3px] w-1/2 origin-center transform bg-atomred transition-all duration-300"></span>
+        </span>
+        <div
           className="md:px-20 py-10 text-lg text-atomgray"
           data-aos="fade-in"
         >
-          I am a <span className="text-atomcyan">second-year</span> student
-          studying <span className="text-atompurple">Engieering Science</span>{" "}
-          at the
-          <span className="text-atompurple"> University of Toronto</span>, with
-          a major in{" "}
-          <span className="text-atompurple">Machine Intelligence</span>. Through
-          this program, I have been exposed to a wide range of core engineering
-          concepts, including quantum mechanics, molecular biology, and fluid
-          dynamics, as well as developing an engineering mindset and
-          problem-solving skills. <br></br>In addition to my schoolwork, I have
-          been actively engaged in extracurricular activities that have allowed
-          me to develop my skills and contribute to the larger student
-          community. I am a Webmaster for the
-          <span className="text-atomyellow">
-            {" "}
-            University of Toronto Engineering Student Consulting Association{" "}
-          </span>
-          and am involved with the
-          <span className="text-atomyellow"> aUToronto </span> student team,
-          working on autonomous vehicle perception. I have also been actively
-          engaged with the
-          <span className="text-atomyellow">
-            {" "}
-            Google Developer Student Club{" "}
-          </span>
-          , supporting and empowering student developers.
+          <Image
+            src={RBC}
+            alt="RBC"
+            height={120}
+            className="md:float-left float-none"
+          />
+          <Image
+            src={Google}
+            alt="Google"
+            height={120}
+            className="md:float-right float-none"
+          />
+          <div>
+            I interned remotely at{" "}
+            <span className="text-atompurple">Royal Bank of Canada (RBC)</span>{" "}
+            in summer <span className="text-atomcyan">2021</span> as a{" "}
+            <span className="text-atomyellow">Innovation Developer</span> as
+            part of the Summer Tech Lab program. During this internship, I built
+            the RBC tech information center, designed the UI components and
+            restructured navigation for the access to the company's tech product
+            using Figma, Angular and TypeScipt. In summer{" "}
+            <span className="text-atomcyan">2022</span>, I returned to{" "}
+            <span className="text-atompurple">RBC</span> as a{" "}
+            <span className="text-atomyellow">Software Developer Intern</span>{" "}
+            to the Toronto Office. I integrated a data hub for RBC online
+            banking across 10 countries and developed a customizable desktop app
+            with React & Electron. For the upcoming summer{" "}
+            <span className="text-atomcyan">2023</span>, I will be joining
+            <span className="text-atompurple"> Google</span> as a{" "}
+            <span className="text-atomyellow">STEP Software Engineer </span>in
+            the Montreal Chrome Team.
+          </div>
         </div>
       </div>
     </div>
