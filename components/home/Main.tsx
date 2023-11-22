56cbe803b6a7bf96585d9b535745385362fcd2f3
Xiao215 <xiaozhang20030215@gmail.com>
2023-11-22 17:12:57 -0500

    add link'

--- components/home/Main.tsx
+++ components/home/Main.tsx
@@ -36,7 +36,7 @@ const Main = () => {
             <Slime width={250} height={150} />
           </div> */}
           <div>
-            https://drive.google.com/drive/folders/1MPLTvOqbkluk2Mgx57qF008kMk8V-d1f?usp=sharing
+            https://drive.google.com/file/d/1Bb2l6fKn4mvDH2OqLwj7ch_G6r6H6J2C/view?usp=sharing
           </div>
           <div className="lg:col-span-3 md:col-span-4 text-lg text-atomgray">
             In the past, I have worked on many full stack projects based using
