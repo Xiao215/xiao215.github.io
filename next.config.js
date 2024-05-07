a5d102a40bb4bfbd86afc6ebcae02e63b43b112c
Xiao215 <xiaozhang20030215@gmail.com>
2024-05-07 16:53:03 -0700

    small update

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
 const withImages = require("next-images");
-module.exports = withImages({
-  experimental: { reactRefresh: false },
-});
+
 module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: "/backend/:path*",
-        destination: "http://127.0.0.1:5000/:path*",
-      },
-    ];
+  output: "export",
+  images: {
+    unoptimized: true,
   },
 };
