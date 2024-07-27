2308348be417c8a34c1eeaaa634dcc1a852b1083
Xiao215 <xiaozhang20030215@gmail.com>
2024-07-26 17:07:52 -0700

    Update Resume Information

--- data/ResumeNavData.tsx
+++ data/ResumeNavData.tsx
@@ -5,33 +5,33 @@ type ResumeNavItem = {
 };
 
 type ResumeNav = {
-  cv: ResumeNavItem;
+  // cv: ResumeNavItem;
   ml: ResumeNavItem;
   swe: ResumeNavItem;
-  fullstack: ResumeNavItem;
+  // fullstack: ResumeNavItem;
 };
 
 const resumeNav: ResumeNav = {
   swe: {
     name: "Software",
-    link: "https://drive.google.com/file/d/1rJ9AEKzpXfqGkq7Qntz5C_jqasoKTRmV/preview",
+    link: "https://drive.google.com/file/d/1eOFepl2Mfsk5X1DANQqtrFkcyQktwZ9u/preview",
     navName: "swe",
   },
   ml: {
     name: "Machine Learning",
-    link: "https://drive.google.com/file/d/1JQ816wmr4muIyqnXOZfv5EvGuiORkB-Z/preview",
+    link: "https://drive.google.com/file/d/1pSJX5jQz3GYFS1BhL_RRn-rhOCGgFJW9/preview",
     navName: "ml",
   },
-  fullstack: {
-    name: "Full Stack",
-    link: "https://drive.google.com/file/d/1Ui9ShPXvTPjllDwVTplmceGV2hr6mnqM/preview",
-    navName: "fullstack",
-  },
-  cv: {
-    name: "CV",
-    link: "https://drive.google.com/file/d/1cIhAMDdPmgg3mNbdxWe0P3RcVBqj2EYM/preview",
-    navName: "cv",
-  },
+  // fullstack: {
+  //   name: "Full Stack",
+  //   link: "https://drive.google.com/file/d/1Ui9ShPXvTPjllDwVTplmceGV2hr6mnqM/preview",
+  //   navName: "fullstack",
+  // },
+  // cv: {
+  //   name: "CV",
+  //   link: "https://drive.google.com/file/d/1cIhAMDdPmgg3mNbdxWe0P3RcVBqj2EYM/preview",
+  //   navName: "cv",
+  // },
 };
 
 export default resumeNav;
